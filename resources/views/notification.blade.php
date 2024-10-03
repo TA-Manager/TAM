40ddbc5d01af8c55290ec439a05a6fe4443b598d
phufaphu <pphufaphu@gmail.com>
2024-10-03 11:06:07 +0700

    feat: add notification(announcement) page 📢

---
+++ resources/views/notification.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <div class="flex justify-center items-center">
+        <p class="font-bold text-3xl text-[#2B308B]">ประกาศ</p>
+    </div>
+    <div>
+        <div class="my-8 mx-24 py-10 px-6 text-[#0692DF] bg-[#0692DF]/[0.1] flex items-center  rounded-[30px]" style="box-shadow: inset 15px -15px 15px 0 rgba(5, 111, 169, 0.1), inset -15px 15px 15px 0 rgba(255, 255, 255, 0.1);">
+            <p class="rounded w-1/2 font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, atque!</p>
+        </div>
+        <div class="my-8 mx-24 py-10 px-6 text-[#0692DF] bg-[#0692DF]/[0.1] flex items-center  rounded-[30px]" style="box-shadow: inset 15px -15px 15px 0 rgba(5, 111, 169, 0.1), inset -15px 15px 15px 0 rgba(255, 255, 255, 0.1);">
+            <p class="rounded w-1/2 font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, atque!</p>
+        </div>
+        <div class="my-8 mx-24 py-10 px-6 text-[#0692DF] bg-[#0692DF]/[0.1] flex items-center  rounded-[30px]" style="box-shadow: inset 15px -15px 15px 0 rgba(5, 111, 169, 0.1), inset -15px 15px 15px 0 rgba(255, 255, 255, 0.1);">
+            <p class="rounded w-1/2 font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, atque!</p>
+        </div>
+        <div class="my-8 mx-24 py-10 px-6 text-[#0692DF] bg-[#0692DF]/[0.1] flex items-center  rounded-[30px]" style="box-shadow: inset 15px -15px 15px 0 rgba(5, 111, 169, 0.1), inset -15px 15px 15px 0 rgba(255, 255, 255, 0.1);">
+            <p class="rounded w-1/2 font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, atque!</p>
+        </div>
+        <div class="my-8 mx-24 py-10 px-6 text-[#0692DF] bg-[#0692DF]/[0.1] flex items-center  rounded-[30px]" style="box-shadow: inset 15px -15px 15px 0 rgba(5, 111, 169, 0.1), inset -15px 15px 15px 0 rgba(255, 255, 255, 0.1);">
+            <p class="rounded w-1/2 font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, atque!</p>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\ApplicationFromController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\NotificationController;
 
 Route::get('/', function () {
     $users = DB::table('users')->select('id','name','email')->orderBy('id', 'asc')->get();
@@ -18,3 +19,4 @@ Route::get('/applicationform', [ApplicationFromController::class, 'showForm']);
 Route::get('/home', [CourseController::class, 'showHome']);
 Route::post('/add-count', [CourseController::class, 'AddCount'])->name('add-count');
 Route::post('/minus-count', [CourseController::class, 'MinusCount'])->name('minus-count');
+Route::get('/notification', [NotificationController::class, 'showNotification']);
